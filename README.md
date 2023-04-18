2214730a6f618d23c9c6af8979712b37c4e3b6ef
ADITYA KAKKAR <adi@ADITYAs-MacBook-Air.local>
2023-04-18 14:26:26 +0530

    rabbit mq connection

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Reminder Service
+//exfzagyjagerzhwv
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+const { createChannel } = require("../src/utils/messageQueue");
 const cron = require("node-cron");
 const TicketController = require("./controllers/ticket-controller");
 // const { sendBasicEmail } = require("./services/email-service");
 const jobs = require("./utils/job");
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
   app.post("/api/v1/tickets", TicketController.create);
   app.listen(PORT, () => {
     console.log(` Server started at port ${PORT}`);
-    jobs();
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
     // sendBasicEmail(
     //   "support@admin.com",
     //   "kakkar.adi14@gmail.com",
